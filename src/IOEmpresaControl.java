8ddcb3a99b2de21b65b0bb791e34474e15d15742
dirwowxd <vicesepulveda2006@gmail.com>
2026-06-02 18:08:07 -0400

    listo

--- src/IOEmpresaControl.java
+++ src/IOEmpresaControl.java
@@ -1,4 +1,104 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class IOEmpresaControl {
+
+    public void registrarArchivoIngreso(String rut, String nombre, LocalTime horaIngresoEmpleado) {
+        LocalDate fecha = LocalDate.now();
+        DateTimeFormatter horaformato = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime horaActual = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String nombreArchivo = "Ingreso_Empleados_Fecha_" + fecha.format(formatter) + ".txt";
+        try (FileWriter fw = new FileWriter(nombreArchivo, true);
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            LocalTime horaIngreso = LocalTime.of(8, 30);
+            DateTimeFormatter hora = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+            String linea = rut + " | " + nombre + " | " + horaIngresoEmpleado;
+            if (horaActual.isAfter(horaIngreso)) {
+                linea += "Atraso";
+                System.out.println(linea);
+            }
+            bw.write(linea);
+            bw.newLine();
+
+        } catch (IOException e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+    }
+
+    public void registrarArchivoSalida(String rut, String nombre, LocalTime horaSalidaEmpleado) {
+        LocalDate fechaActual = LocalDate.now();
+        DateTimeFormatter fechaFormato = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalTime horaActual = LocalTime.now();
+        DateTimeFormatter horaformato = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String nombreArchivo = "Ingreso_Empleados_Fecha_" + fechaActual.format(horaformato) + ".txt";
+        try (FileWriter fw = new FileWriter(nombreArchivo, true);
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            LocalTime horaSalida = LocalTime.of(17, 0);
+            DateTimeFormatter horaFormato = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String linea = rut + " | " + nombre + " | " + horaSalidaEmpleado;
+            if (horaActual.isAfter(horaSalida)) {
+                Duration duracion = Duration.between(horaSalida, horaActual);
+                long horasExtras = duracion.toHours();
+                long minutosExtras = duracion.toMinutesPart();
+                linea += "Hora extras " + horasExtras + " minutos " + minutosExtras;
+                System.out.println(linea);
+            }
+            bw.write(linea);
+            bw.newLine();
+            System.out.println("Registro de horario de salida exitosamente creado");
+        } catch (IOException e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+    }
+
+
+    public void leerHorariosEntrada() {
+        LocalDate fechaActual = LocalDate.now();
+        DateTimeFormatter fechaFormato = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String nombreArchivo = "Ingreso_Empleados_Fecha_" + fechaActual.format(fechaFormato) + ".txt";
+        File file = new File(nombreArchivo);
+        if (!file.exists()) {
+            System.out.println("El archivo no existe");
+            return;
+        }
+        try (FileReader fr = new FileReader(file);
+             BufferedReader br = new BufferedReader(fr)) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println("\n--- REGISTROS DE INGRESO DEL DÍA ---");
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+
+            System.out.println("Error : " + e.getMessage());
+        }
+
+    }
+
+    public void leerHorariosSalidas() {
+        LocalDate fechaActual = LocalDate.now();
+        DateTimeFormatter fechaFormato = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String nombreArchivo = "Salida_Empleados_Fecha_" + fechaActual.format(fechaFormato) + ".txt";
+        File file = new File(nombreArchivo);
+        if (!file.exists()) {
+            System.out.println("El archivo no existe");
+            return;
+        }
+        try (FileReader fr = new FileReader(file);
+             BufferedReader br = new BufferedReader(fr)) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println("\n--- REGISTROS DE SALIDA DEL DÍA ---");
+                System.out.println(linea);
+
+            }
+        } catch ( IOException e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        IOEmpresaControl Control = new IOEmpresaControl();
+        int opcion;
+        do {
+            System.out.println("MENU");
+            System.out.println("1. Registrar entrada empleado");
+            System.out.println("2. Registrar salida empleado");
+            System.out.println("3. Leer entrada empleado");
+            System.out.println("4. Leer salida empleado");
+            System.out.println("5. Salir");
+            System.out.print("Opcion: ");
+            opcion = sc.nextInt();
+            sc.nextLine();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Rut");
+                    String rut= sc.nextLine();
+                    System.out.println("Nombre ");
+                    String nombre= sc.nextLine();
+                    System.out.println("Horario de entrada");
+                    LocalTime horario= LocalTime.parse(sc.nextLine());
+                    Control.registrarArchivoIngreso(rut, nombre, horario);
+                    break;
+                case 2:
+                    System.out.println("Rut");
+                    String rutSalida= sc.nextLine();
+                    System.out.println("Nombre ");
+                    String nombreSalida= sc.nextLine();
+                    System.out.println("Horario de entrada");
+                   LocalTime horarioSalida= LocalTime.parse(sc.nextLine());
+                   Control.registrarArchivoSalida(rutSalida, nombreSalida, horarioSalida);
+                    break;
+                case 3:
+                    Control.leerHorariosEntrada();
+                    break;
+                case 4:
+                    Control.leerHorariosSalidas();
+                    break;
+                case 5:
+                    System.out.println("Salida");
+
+            }
+        } while (opcion != 5);
+        sc.close();
+    }
 }
